b42395a302f83aee805e31281341816acd079dd2
zli <30396815+zli42@users.noreply.github.com>
2022-07-26 23:54:56 +0800

    Initialize ts/main

---
+++ src/__tests__/app.tsx
@@ -0,0 +1,22 @@
+// Commenting test out as it seems currently polkadot-js api is not compatible with jest
+//   and will always cause `SyntaxError: Cannot use import statement outside a module`.
+// See: https://github.com/polkadot-js/api/issues/3430
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from '../App';
+
+describe('App Test Suite', () => {
+  it('renders without crashing', () => {
+    // const div = document.createElement('div');
+    // ReactDOM.render(<App />, div);
+<<<<<<< HEAD:src/__tests__/app.tsx
+  });
+});
+
+// Empty export is required if there are no imports
+export { };
+=======
+  })
+})
+>>>>>>> b58f4db4e023ddcd9cdb7f62cd84860ee47c8eb1:src/__tests__/App.js
